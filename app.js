b11cdfa5c7d15727d6c9458e9a4fc5b86ad1fbbf
Trishtan Adhikari <adhikaritrishtan77@gmail.com>
2025-01-13 22:27:32 +0545

    initialize project with Express, EJS, and basic structure

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT;
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname ,'public')));
+
+
+
+app.get('/', (req,res) =>{
+    res.render('index');
+})
+
+
+
+app.listen(PORT,()=>{
+    console.log(`app is listening at port ${PORT}`)
+})
+

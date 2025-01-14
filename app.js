f5a0a54156ef89c12196c72ff68155b80bfb9959
Trishtan Adhikari <adhikaritrishtan77@gmail.com>
2025-01-14 16:39:02 +0545

    add user edit functionality with update form and routes

--- app.js
+++ app.js
@@ -42,6 +42,24 @@ app.get('/delete/:id' , async (req , res)=>{
    res.redirect('/read')
 })
 
+app.get('/edit/:userid', async (req,res) => {
+   
+ let user =  await userModel.findOne({_id: req.params.userid});
+
+ res.render('edit', {user});
+})
+
+app.post('/update/:userid', async (req,res)=>{
+    let {name , email , image}= req.body
+let user =   await userModel.findOneAndUpdate({_id: req.params.userid}, {
+    name,
+    email,
+    image
+  } )
+
+  res.redirect('/read')
+})
+
 app.listen(PORT,()=>{
     console.log(`app is listening at port ${PORT}`)
 })
--- models/user.model.js
+++ models/user.model.js
@@ -7,6 +7,7 @@ const userSchema = mongoose.Schema({
     name: String,
     email: String,
     image: String
+    
 })
 
 module.exports = mongoose.model('user', userSchema)
